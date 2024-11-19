cf77f48ae56b36cb4bce81cb8a53dfd47c877438
MikeMordec <109396910+MikeMordec@users.noreply.github.com>
2024-11-19 02:01:38 -0600

    Create MarketSimulation.java

---
+++ MarketSimulation.java
@@ -0,0 +1,9 @@
+public class MarketSimulation {
+
+    public static void simulateMarket() {
+        // Random price changes (mock example)
+        double priceChange = Math.random() * 10 - 5; // Random change between -5 and +5
+        System.out.println("Market simulation: Stock prices fluctuating by " + priceChange);
+        // Update stock prices accordingly (you can choose how to apply this logic)
+    }
+}
